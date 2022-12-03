c4b3aa2a1f3dce4501b3e4617ed7105ab3b84814
KP-Svitlana <kovalenko.svitlana.p@gmail.com>
2022-12-03 17:06:12 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
 
+stopBtn.disabled = true;
+
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick(e) {
+  const intervalId = setInterval(() => {
+    body.style.backgroundColor = `${getRandomHexColor()}`;
+  }, 1000);
+  localStorage.setItem('intervalId', intervalId);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+}
+
+function onStopBtnClick() {
+  clearInterval(JSON.parse(localStorage.getItem('intervalId')));
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
